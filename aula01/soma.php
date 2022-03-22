e83cef12a0d06b9c9f6c01b26a6123e8387c739e
J.J <99402892+JuliaC261@users.noreply.github.com>
2022-03-22 09:13:49 -0300

    Create soma.php

---
+++ aula01/soma.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+  </head>
+  <body>
+
+<a href="index.php">Voltar</a>
+    
+  <?php 
+$n1 = $_POST["n1"];
+$n2 = $_POST["n2"];
+$resultado = $n1 + $n2;
+echo "Resultado:".$resultado;
+?>
+
+  </body>
+</html>

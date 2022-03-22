2696683717aba23f4a3726270edaea934c550bd8
J.J <99402892+JuliaC261@users.noreply.github.com>
2022-03-22 09:11:47 -0300

    Create index2.php

---
+++ aula01/index2.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    
+ <form action="soma.php" method="post">
+   Número 1: <input type="text" name="n1"><br/>
+   Número 2: <input type="text" name="n2"><br/>
+   <input type="submit" value="calcular"> <br/>
+ </form>
+
+    <a href="../aula01/index2.php">Voltar</a> <br>
+ <a href=-"index2.php">Limpar</a>
+  
+  </body>
+</html>

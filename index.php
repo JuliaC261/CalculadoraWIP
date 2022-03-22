943581f5079a2de4ac86980c10d5cc46bdcd1642
J.J <99402892+JuliaC261@users.noreply.github.com>
2022-03-22 09:10:58 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+
+<h2>Aulas</h2>
+
+<label>Aula 01</label> 
+    <a href="aula01/index2.php">Clique Aqui</a>
+    
+ <form action= "calcular.php" method= "POST"
+   
+<label>Aula 02</label> 
+    <a href="aula02/index.php">Clique Aqui</a>
+    
+
+</form>
+  
+  </body>
+</html>
